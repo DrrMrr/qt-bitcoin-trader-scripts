d1ea297409446ebef91e4e66ae64b326546714ab
Damjan <pisi.na@gmail.com>
2017-07-22 10:24:50 +0200

    Update TraderMain.js

--- TraderMain.js
+++ TraderMain.js
@@ -1,43 +1,43 @@
-var variablePath = "/home/damien/Desktop/QTBitcoinTrader/Variables/";
+var variablePath = "/home/damien/Desktop/QTBitcointTrader/";
 ///////////////////////////////////////////////////////////////////
 // in%,the first step indentation How price first buy order in the table should be less than the purchase of the current at the time of the calculation table.  All other orders will buy more cheaper //default 0.5
-var otstupFile = variablePath + "firstBuy.txt";
+var otstupFile = variablePath + "FirstBuy.txt";
 var otstup = parseFloat(trader.fileReadAll(otstupFile));
 ///////////////////////////////////////////////////////////////////
 //NumberOfOrders.txt
-var ordersFile = variablePath + "numberOfOrders.txt";
+var ordersFile = variablePath + "NumberOfOrders.txt";
 var orders = parseFloat(trader.fileReadAll(ordersFile)); // 2 to 20,how many buy-to place orders in the amount of overlap. //default 13
 ///////////////////////////////////////////////////////////////////
 //StepBetweenOrders.txt
-var perekrFile = variablePath + "stepBetweenOrders.txt";
+var perekrFile = variablePath + "StepBetweenOrders.txt";
 var perekr = parseFloat(trader.fileReadAll(perekrFile)); // in% overlap stroke rates,calculating the depth of the table buy orders.  For example,in a first step,the price 10ETH percentage of overlap of 20% means that the table will be set in the range // 8-10ETH.  //default 30
 ///////////////////////////////////////////////////////////////////
 //Profit.txt
-var profitFile = variablePath + "profit.txt";
+var profitFile = variablePath + "Profit.txt";
 var profit = parseFloat(trader.fileReadAll(profitFile)); // in%,profit,profit How to lay into each sell order. 
 ///////////////////////////////////////////////////////////////////
 //Margin.txt
-var martinFile = variablePath + "martin.txt";
+var martinFile = variablePath + "Martin.txt";
 var martin = parseFloat(trader.fileReadAll(martinFile)); // in%,martingale,when calculating each table buy orders following order cheaper by volume greater than the previous to this value.  //default 15
 ///////////////////////////////////////////////////////////////////
-//lastSale.txt
-var lastSaleFile = variablePath + "_logLastSale.txt";
+
+var lastSaleFile = variablePath + "lastSale.txt";
 trader.fileWrite(lastSaleFile,0);
 var lastSale = parseFloat(trader.fileReadAll(lastSaleFile)); // in%,martingale,when calculating each table buy orders following order cheaper by volume greater than the previous to this value.  //default 15
 ///////////////////////////////////////////////////////////////////
-//lastETHbalance.txt
-var lastETHbalanceFile = variablePath + "_logLastETHbalance.txt";
+
+var lastETHbalanceFile = variablePath + "lastETHbalance.txt";
 trader.fileWrite(lastETHbalanceFile,0);
 var lastETHbalance = parseFloat(trader.fileReadAll(lastETHbalanceFile));
 ///////////////////////////////////////////////////////////////////
-//logBidPrice.txt
-var bidPriceFile = variablePath + "_logBidPrice.txt";
+
+var bidPriceFile = variablePath + "bidPrice.txt";
 var bidPrice = trader.get("BidPrice");
 trader.fileWrite(bidPriceFile,bidPrice);
 ///////////////////////////////////////////////////////////////////
 
-//var perekrDif = 0.25;
-//var martinDif = 0.1;
+var perekrDif = 0.25;
+var martinDif = 0.1;
 
 var numberOfBids = 0;
 var numberOfAsks = trader.get("OpenAsksCount");
