328a2cc2ac5c991830e9ef2b518289341418f25a
Damjan Semenič <damjan,semenic@outlook.com>
2017-11-02 22:01:20 +0100

    minor corrections for variable minBidAmount

--- Trader.js
+++ Trader.js
@@ -138,7 +138,7 @@ var allBidsPriceEnabled = trader.fileReadAll(allBidsPriceEnabledFile).toString()
 ///////////////////////////////////////////////////////////////////
 var currencyPrimaryKeepAmountFixedValue = 0;
 var currencySecondaryKeepAmountFixedValue = 0.15;
-var minBidAmount = 0.04;
+var minBidAmount = 0.004;
 
 
 ///////////////////////////////////////////////////////////////////
@@ -799,12 +799,17 @@ function makeAsk() {
             lastCurrencySecondaryBallance = tempValue;
             makeSell = true;
         }
-		
-		if((lastCurrencySecondaryBallance - currencySecondaryKeepAmountFixedValue - minBidAmount) < 0)
+
+        ////////////////////////////////////////////////////////////////////////////////
+        var tempValue3 = 0;  
+        tempValue3 = lastCurrencySecondaryBallance - minBidAmount * (1 - feeMaker);
+        eventLogger(scriptName + ".tempValue3: " + tempValue3);
+		if(tempValue3 < 0)
 		{
 			eventLogger(scriptName + ".STEP 0.3");
 			makeSell = false;
-		}
+        }
+        ////////////////////////////////////////////////////////////////////////////////
 			
     } else {
         eventLogger(scriptName + ".STEP 0.3");
